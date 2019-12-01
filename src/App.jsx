5b7efb904bc4fc51d4906ece174d66059970b3b2
Dariusz Okonski <darek200180@gmail.com>
2019-12-01 23:00:54 +0000

    add BowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import appStyles from './css/App.module.css';
+import Game from './components/Game';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className={appStyles.app}>
-      <h1>Hello world</h1>
+      <h1 className={appStyles.header}>Lotto App</h1>
+      <BrowserRouter>
+        <Game />
+      </BrowserRouter>
     </div>
   );
 }
