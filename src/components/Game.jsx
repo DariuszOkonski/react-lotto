f6dbb454d6ddd6ae35eb011d71c437ef75d27ef0
Dariusz Okonski <darek200180@gmail.com>
2019-12-01 23:40:44 +0000

    add component Game

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import gameStyles from '../css/Game.module.css';
 import Options from './Options';
+import Draw from './Draw';
 
 const Game = () => {
   return (
     <section className={gameStyles.game}>
-      <Options />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Options} />
+          <Route exact path='/lotto' component={() => <Draw balls={6} pot={49} />} />
+          <Route exact path='/eurojackpot' component={() => <Draw balls={5} pot={50} />} />
+          <Route path='/minilotto' component={() => <Draw balls={5} pot={42} />} />
+        </Switch>
+      </BrowserRouter>
     </section>
   );
 }
