573124ad9d813dbeed581a637409f4085f5a474f
Dariusz Okonski <darek200180@gmail.com>
2019-12-02 00:56:03 +0000

    write a logic for drawing a new balls

--- src/components/Draw.jsx
+++ src/components/Draw.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import optionsStyles from '../css/Options.module.css';
 import drawStyles from '../css/Draw.module.css';
-import Ball from './Ball';
+import BallsList from './BallsList';
 
 const Draw = (props) => {
+  const [arr, setArr] = useState([]);
+  const [done, setDone] = useState(false);
+
+  const generate = () => {
+    let rangeArr = [];
+    let arr = [];
+
+    for (let i = 0; i < props.pot; i++) {
+      rangeArr.push(i + 1);
+    }
+
+    for (let i = 0; i < props.balls; i++) {
+      let newNumber = rangeArr.splice(Math.floor(Math.random() * rangeArr.length), 1);
+      arr.push(newNumber[0]);
+    }
+
+    setArr([...arr]);
+    setDone(true);
+  }
+
   return (
     <section className={drawStyles.draw}>
       <h3 className={drawStyles.header}>Draw {props.balls} balls from pot of {props.pot}</h3>
 
-      <Ball number={4} />
+      <BallsList arr={arr} />
 
-      <button className={optionsStyles.button}>Generate</button>
+      <button
+        disabled={done}
+        className={optionsStyles.button}
+        onClick={generate}
+      >Generate</button>
+
       <Link className={optionsStyles.button} to='/'>Back</Link>
     </section>
   );
